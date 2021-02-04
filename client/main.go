5ba1a89d070ded010990c9c84fe03598ec4256f8
Quentin Kniep <hello@quentinkniep.com>
2021-02-04 09:47:04 +0100

    ✨ basic file upload works

--- client/main.go
+++ client/main.go
@@ -5,17 +5,34 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"strconv"
 )
 
 func main() {
+	//search("Test")
+	//download(0, "test.pdf")
+	upload("/Users/qkniep/Downloads/Armada.pdf")
 }
 
 func search(query string) {
+	resp, err := http.Get("localhost:9898/books?query=" + query)
+	if err != nil {
+		fmt.Println("Error: Failed to contact server.")
+		return
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error: Failed to read response body.")
+		return
+	}
+	fmt.Println(string(body))
 }
 
 func downloadToKindle(bookID int) {
@@ -24,10 +41,10 @@ func downloadToKindle(bookID int) {
 
 func download(bookID int, destination string) {
 	f, err := os.Create(destination)
-	defer f.Close()
 	if err != nil {
 		fmt.Println("Error: Failed to create file on Kindle.")
 	}
+	defer f.Close()
 	writer := bufio.NewWriter(f)
 	resp, err := http.Get("localhost:9898/books/" + strconv.Itoa(bookID))
 	if err != nil {
@@ -39,10 +56,39 @@ func download(bookID int, destination string) {
 // Uploads the eBook file to the server.
 func upload(filename string) {
 	f, err := os.Open(filename)
-	defer f.Close()
 	if err != nil {
 		fmt.Println("Error: Failed to open eBook file.")
 	}
-	reader := bufio.NewReader(f)
-	http.Post("localhost:9898/upload", "application/octet-stream", reader)
+	defer f.Close()
+
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+	fw, err := w.CreateFormFile("ebook", f.Name())
+	if err != nil {
+		fmt.Println("Error: Failed to create form file.")
+		return
+	}
+	if _, err = io.Copy(fw, f); err != nil {
+		fmt.Println("Error: Failed to copy file contents.")
+		return
+	}
+	w.Close()
+
+	req, err := http.NewRequest("POST", "http://localhost:9898/upload", &b)
+	if err != nil {
+		fmt.Println("Error: Failed to create request.")
+		return
+	}
+	req.Header.Set("Content-Type", w.FormDataContentType())
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error: Failed to contact server.")
+		return
+	}
+
+	if res.StatusCode != http.StatusOK {
+		fmt.Println("Error: Bad status", res.Status)
+	}
 }
--- server/main.go
+++ server/main.go
@@ -3,5 +3,53 @@
 
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"io"
+	"io/ioutil"
+	"log"
+)
+
+const maxUploadFileSize = 50 << 20 // 50 MiB
+
 func main() {
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/upload", uploadFile)
+
+	log.Println("INFO: Server now listening.")
+	log.Fatal(http.ListenAndServe(":9898", nil))
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		log.Println("WARN: The /upload endpoint only supports POST.")
+		return
+	}
+	log.Println("INFO: User is uploading a new eBook file.")
+
+	// grab the file from the multipart form
+	r.ParseMultipartForm(maxUploadFileSize)
+	file, handler, err := r.FormFile("ebook")
+	if err != nil {
+		log.Println("Error Retrieving the File:", err)
+		return
+	}
+	defer file.Close()
+	log.Println("Uploaded File:", handler.Filename)
+	log.Println("File Size:", handler.Size)
+	log.Println("MIME Header:", handler.Header)
+
+	// store it in a temporary file
+	tempFile, err := ioutil.TempFile("/tmp", "upload-*.pdf")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer tempFile.Close()
+	io.Copy(tempFile, file)
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
 }
